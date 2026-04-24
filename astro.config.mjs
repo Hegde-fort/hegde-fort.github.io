2657fd7d227355f8c70503146a9ab7932d34c5bd
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2026-04-24 10:58:24 -0600

    chore: rename repo to use base GitHub page name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Hegde-fort.github.io',
-  base: 'fort_website',
+  site: 'https://hegde-fort.github.io',
 });
